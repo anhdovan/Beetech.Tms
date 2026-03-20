1f81c077180e6d7dba654e85c214741f995926fa
anhdovan <dvanh1974@yahoo.com>
2026-03-20 17:26:17 +0700

    Update: Enhanced DataSeeder with department assignments and sample transactions (Laundry and Department Delivery)

--- Beetech.Tms.Web/Services/DataSeeder.cs
+++ Beetech.Tms.Web/Services/DataSeeder.cs
@@ -120,17 +120,114 @@ public class DataSeeder
             // Seed 3 items for each category
             for (int i = 1; i <= 3; i++)
             {
-                var tag = $"E200{category.Id:D3}{i:D2}";
+                var dept = departments[random.Next(departments.Length)];
                 await _context.TextileItems.AddAsync(new TextileItem
                 {
                     CategoryId = category.Id,
                     Status = ItemStatus.Available,
                     CurrentLocationId = mainLocation,
+                    CurrentDepartmentId = dept.Id,
                     WashCount = random.Next(0, 10),
                     LastScanAt = DateTime.UtcNow
                 });
             }
         }
+
+        await _context.SaveChangesAsync();
+
+        // Seed some Transactions
+        var allItems = await _context.TextileItems.ToListAsync();
+        var laundryLocation = (await _context.Locations.FirstOrDefaultAsync(l => l.Type == LocationType.Laundry))?.Id;
+        
+        var transactions = new List<Transaction>();
+        
+        // 1. Send items to Laundry
+        var tx1 = new Transaction
+        {
+            TransactionNumber = $"TX-{DateTime.UtcNow:yyyyMMdd}-001",
+            Type = TransactionType.LaundrySend,
+            FromLocationId = mainLocation,
+            ToLocationId = laundryLocation,
+            TransactionDate = DateTime.UtcNow.AddDays(-1),
+            Notes = "Seeded: Sending to laundry"
+        };
+        
+        // Pick 5 random items for this transaction
+        var txItems1 = allItems.OrderBy(x => Guid.NewGuid()).Take(5).Select(item => new TransactionItem
+        {
+            TextileItemId = item.Id,
+            StatusAtTransaction = ItemStatus.Soiled
+        }).ToList();
+        
+        foreach (var ti in txItems1) tx1.Items.Add(ti);
+        transactions.Add(tx1);
+
+        // Update item status for these 5 items
+        foreach (var ti in txItems1)
+        {
+            var textile = allItems.First(i => i.Id == ti.TextileItemId);
+            textile.Status = ItemStatus.Soiled;
+            textile.CurrentLocationId = laundryLocation; 
+        }
+
+        // 2. Receive from Laundry
+        var tx2 = new Transaction
+        {
+            TransactionNumber = $"TX-{DateTime.UtcNow:yyyyMMdd}-002",
+            Type = TransactionType.LaundryReceive,
+            FromLocationId = laundryLocation,
+            ToLocationId = mainLocation,
+            TransactionDate = DateTime.UtcNow.AddHours(-2),
+            Notes = "Seeded: Received from laundry"
+        };
+
+        // Pick 3 other random items for this
+        var txItems2 = allItems.OrderBy(x => Guid.NewGuid()).Take(3).Select(item => new TransactionItem
+        {
+            TextileItemId = item.Id,
+            StatusAtTransaction = ItemStatus.Available
+        }).ToList();
+        
+        foreach (var ti in txItems2) tx2.Items.Add(ti);
+        transactions.Add(tx2);
+
+        // Update item status
+        foreach (var ti in txItems2)
+        {
+            var textile = allItems.First(i => i.Id == ti.TextileItemId);
+            textile.Status = ItemStatus.Available;
+            textile.CurrentLocationId = mainLocation;
+            textile.WashCount++;
+        }
+
+        await _context.Transactions.AddRangeAsync(transactions);
+
+        // 3. Delivery to Department
+        var dept1 = departments[0];
+        var tx3 = new Transaction
+        {
+            TransactionNumber = $"TX-{DateTime.UtcNow:yyyyMMdd}-003",
+            Type = TransactionType.Delivery,
+            FromLocationId = mainLocation,
+            DepartmentId = dept1.Id,
+            TransactionDate = DateTime.UtcNow.AddMinutes(-30),
+            Notes = $"Seeded: Delivery to {dept1.Name}"
+        };
+
+        var txItems3 = allItems.Where(i => i.Status == ItemStatus.Available).OrderBy(x => Guid.NewGuid()).Take(4).Select(item => new TransactionItem
+        {
+            TextileItemId = item.Id,
+            StatusAtTransaction = ItemStatus.InUse
+        }).ToList();
+
+        foreach (var ti in txItems3) tx3.Items.Add(ti);
+        foreach (var ti in txItems3)
+        {
+            var textile = allItems.First(i => i.Id == ti.TextileItemId);
+            textile.Status = ItemStatus.InUse;
+            textile.CurrentDepartmentId = dept1.Id;
+        }
+        await _context.Transactions.AddAsync(tx3);
 
         await _context.SaveChangesAsync();
     }
